31741fe3533a46d8d245e27611f521433e02275a
Esteb4ncd <davidecruzd10@gmail.com>
2025-09-24 18:38:54 -0700

    Clean commit: ignore .history and add current project

--- .history/src/components/Book_20250919120917.js
+++
@@ -1,9 +0,0 @@
-function Book({ book }) {
-  return (
-    <div className="book">
-      <img src={book.image} alt={book.title} className="book-image" />
-      <h3>{book.title}</h3>
-      <p className="price">{book.price}</p>
-    </div>
-  );
-}
